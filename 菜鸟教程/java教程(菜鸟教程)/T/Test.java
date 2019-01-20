608414a60d0b68a40b6cccaa9106d08d90859f76
Greenbirch2007 <291109028@qq.com>
2019-01-20 19:43:06 +0800

    １５５个实例加上操作mysql

---
+++ 菜鸟教程/java教程(菜鸟教程)/T/Test.java
@@ -0,0 +1,12 @@
+
+
+/** 返回两个整型变量数据的较大值 */
+public static int max(int num1, int num2) {
+        int result;
+        if (num1 > num2)
+        result = num1;
+        else
+        result = num2;
+
+        return result;
+        }t
